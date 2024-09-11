44e735b18daa051f4d66cd5c500ed9c318622514
Пользователь <milya.nuriyeva@gmail.com>
2024-09-11 12:07:56 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+
+URL_SERVICE = "https://e2a91221-947f-4656-8f27-274b63e7cb87.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,27 @@
+# Импорт настроек из модуля configuration, который содержит параметры конфигурации, такие как URL сервиса
+import configuration
+
+# Импорт библиотеки requests для выполнения HTTP-запросов
+import requests
+
+# Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+response = post_products_kits(data.product_ids)
+print(response.status_code)
+print(response.json())
